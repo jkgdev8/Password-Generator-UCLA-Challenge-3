9d2333189efe779a6948c83b07ade81f4e6a8df8
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2022-02-04 20:38:02 -0800

    updated function

--- script.js
+++ script.js
@@ -1,47 +1,92 @@
-var upLetters = ["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowLetters = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var nums = ['1', '2', '3',"4", "5", "6", "7", "8","9","0"];
-var specialChars = ['!', '@', "#", "$", "%", "^", "&", "*", "(", ")", "-","_", "=", "+"];
-var Chars = upLetters + lowLetters + nums + specialChars ;
-var counter = 1;
-
-// Assignment code here
-
-window.prompt("How many characters would you like your passwords to contain?");
-window.confirm("Click OK to confirm including special characters");
-window.confirm("Click OK to confirm including numeric characters");
-window.confirm("Click OK to confirm including uppercase characters");
-
+// The Password generator will provide a password with 8-128 characters based on user selections
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+// pwd can include alphanumeric and special characters
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialChar = [" ", ".", "~", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", "/", "^", ",", ":", ";", "<", ">", "=", "?", "@", "[", "]", "|", "{", "}", "_", "`"];
+let alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// variables for criteria to include in the password
+let confirmLength = "";
+let confirmSpecialCharacter;
+let confirmNumericCharacter;
+let confirmUpperCase;
+let confirmLowerCase;
+
+// store the result in an array
+let pwdChars = [];
+
+// function to generate a random pwd
+let generatePassword = function () {
+    confirmLength = window.prompt("How many characters would you like your password to contain?");
+
+    // if length of pwd is outside pwd requirements display an alert to the user
+    while ((confirmLength <= 7) || (confirmLength >= 129)) {
+        window.alert("Password length must be between 8 - 128 characters Try again");
+        confirmLength = window.prompt("How many characters would you like your password to contain?");
+    }
+
+    // confirm pwd length to the user
+    window.alert(`Your password will have ${confirmLength} characters`);
+
+    // confirm character types to include in the pwd
+    confirmSpecialCharacter = window.confirm("Click OK to confirm if you would like to include special characters");
+    confirmNumericCharacter = window.confirm("Click OK to confirm if you would like to include numeric characters");
+    confirmLowerCase = window.confirm("Click OK to confirm if you would like to include lowercase characters");
+    confirmUpperCase = window.confirm("Click OK to confirm if you would like to include uppercase characters");
+
+    // validating input - least one character type should be selected
+    while (confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+        alert("You must choose at least one character type");
+        confirmSpecialCharacter = window.confirm("Click OK to confirm if you would like to include special characters");
+        confirmNumericCharacter = window.confirm("Click OK to confirm if you would like to include numeric characters");
+        confirmLowerCase = window.confirm("Click OK to confirm if you would like to include lowercase characters");
+        confirmUpperCase = window.confirm("Click OK to confirm if you would like to include uppercase characters");
+    }
+
+    /*
+        Could use concat instead of the spread operator
+            pwdChars = pwdChars.concat(specialChar)
+     */
+
+    // resulting pwd string
+    let randomPwd = "";
+
+    if (confirmSpecialCharacter) {
+        pwdChars = [...pwdChars, ...specialChar];
+    }
+
+    if (confirmNumericCharacter) {
+        pwdChars = [...pwdChars, ...number];
+    }
+
+    if (confirmLowerCase) {
+        pwdChars = [...pwdChars, ...alphaLower];
+    }
+
+    if (confirmUpperCase) {
+        pwdChars = [...pwdChars, ...alphaUpper];
+    }
+
+    // password is generated that matches the selected criteria
+    for (let i = 0; i < confirmLength; i++) {
+        randomPwd = randomPwd + pwdChars[Math.floor(Math.random() * pwdChars.length)];
+        console.log(randomPwd)
+    }
+    return randomPwd;
+};
 
 // Write password to the #password input
-//function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("password");
+function writePassword() {
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
 
-  //passwordText.value = password;
+    passwordText.value = password;
 
-//}
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-
-function getRandomLetter() {
-  
-  var lastIndex = Chars.length-1
-  // a random number between 0 - lastIndex
-  const randNum = Math.floor(Math.random() * (lastIndex + 1))
-  var letter = Chars[randNum]
-  return letter
 }
 
-console.log(getRandomLetter([Chars]));
-
-///////////////////////
-
-
-
-
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
